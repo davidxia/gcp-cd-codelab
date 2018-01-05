e599513acc46f8dd12960ec2965e13d279cc112d
David Xia <dxia@spotify.com>
2018-01-05 13:10:00 -0500

    Change message again

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "testing deploying to multiple clusters")
+	io.WriteString(w, "testing deploying to multiple clusters. Will this work?")
 }
 
 func main() {
